b091508f57d2f8a7925513a7aa01cee0e8db95f6
simonjujuh <juhel.simon6@gmail.com>
2024-07-27 21:02:11 +0200

    Update user setup script and custom files

--- setup/load_user_setup.sh
+++ setup/load_user_setup.sh
@@ -20,3 +20,68 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm || true
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
 apt update && apt install sublime-text
+
+# Install exa, replacement for ls
+export DEBIAN_FRONTEND=noninteractive 
+apt-get install --yes exa
+
+# Nxc configuration
+cp -r /opt/my-resources/setup/nxc/nxc.conf /root/.nxc/nxc.conf
+
+# Permissions fix
+find /workspace/ -type d -exec chmod 770 {} \; -exec chmod g+s {} \;
+find /workspace/ -type f -exec chmod 660 {} \;
+
+
+# The /opt/supported_setups.md file lists the supported configurations that can be made easily.
+# LAUNCH_DIRECTORY="$PWD"
+
+# # remove history
+# echo -n > ~/.zsh_history
+
+# pentest environment variables source
+# FILE=/workspace/.env
+# if [ ! -f $FILE ]
+# then
+#     touch $FILE
+# fi
+# echo "CLIENT=''" >> $FILE
+# echo "DOMAIN=''" >> $FILE
+# echo "FQDN=''" >> $FILE
+# echo "DC=''" >> $FILE
+# echo "DC2=''" >> $FILE
+# echo "DC3=''" >> $FILE
+# echo "USER=''" >> $FILE
+# echo "PASSWORD=''" >> $FILE
+# echo "USER_ADM=''" >> $FILE
+# echo "PASSWORD_ADM=''" >> $FILE
+
+# # build internal pentest tree
+# mkdir /workspace/attacks
+# mkdir /workspace/attacks/adcs
+# mkdir /workspace/attacks/crack
+# mkdir /workspace/attacks/cve
+# mkdir /workspace/attacks/acl
+# mkdir /workspace/attacks/mitm
+# mkdir /workspace/attacks/postex
+# mkdir /workspace/attacks/relay
+# mkdir /workspace/attacks/snowball
+# mkdir /workspace/attacks/spray
+# mkdir /workspace/attacks/webdav
+# mkdir /workspace/enumeration
+# mkdir /workspace/enumeration/bloodhound
+# mkdir /workspace/enumeration/nmap
+# mkdir /workspace/enumeration/pingcastle
+# mkdir /workspaces/enumeration/nessus
+# mkdir /workspace/loot
+# mkdir /workspace/loot/files
+# mkdir /workspace/loot/credz
+# mkdir /workspace/notes
+# touch /workspace/notes/brief.md
+# touch /workspace/notes/todo.md
+# touch /workspace/notes/comptes.md
+# touch /workspace/notes/partages.md
+# touch /workspace/notes/questions.md
+# touch /workspace/notes/postex.md
+# mkdir /workspace/screenshots
+# mkdir /workspace/targets
